ee0c04a0531836e6d67d6e45c7e8c49c92d0ddcb
jake-abed <jakeabed@gmail.com>
2023-02-04 15:02:01 -0800

    Continue setup, add module objects & basic CSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//Create game state object
+const gameState = (() => {
+	let preGame = true,
+		gameActive = false,
+		player1Score = 0,
+		player2Score = 0; 
+
+	const startGame = () => {
+		preGame = false;
+		gameActive = true;
+		player1Score = 0;
+		player2Score = 0;
+	}
+
+	return {
+		preGame,
+		startGame
+	};
+})();
+
+//Create gameboard object
+
+//Create player objects - factories
+
+//Create functions to render gameboard.
